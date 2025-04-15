2a1d648d3ef7a74061f7e45a148a4f5167b99265
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-04-15 11:37:22 +0530

    app: move Home code to a client component
    to set metadata in app router, we need to export metadata, which can only
    be done in a server component
    So we move the whole code to a server component
    
    Signed-off-by: John Titor <50095635+JohnRTitor@users.noreply.github.com>

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,11 @@
-"use client"; // Ensures this component is rendered on the client side
+import HomeClientSection from "@/components/HomeClientSection";
+import { Metadata } from "next";
 
-import Head from "next/head";
-import CommonHeader from "@/components/Header";
-import SimpleStorageSection from "@/components/SimpleStorageSection";
-import { useAccount } from "wagmi";
-import { useEffect, useState } from "react";
+export const metadata: Metadata = {
+  title: "Simple Storage Contract Demo",
+  description: "Demo for interacting with the SimpleStorage contract",
+};
 
 export default function Home() {
-  // Check if the user is connected to a wallet
-  const { isConnected } = useAccount();
-
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      {/* Set page metadata */}
-      <Head>
-        <title>Simple Storage Contract Demo!</title>
-        <meta name="description" content="Demo for interacting with the SimpleStorage contract" />
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      {/* Header with wallet connect/disconnect button */}
-      <CommonHeader />
-
-      {/* Main section of the homepage */}
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <h2 className="text-2xl font-semibold">Welcome to the SimpleStorage DApp</h2>
-
-        {/* Show message or main content depending on connection and mounting status */}
-        {!isConnected ? (
-          <p className="text-gray-500">Connect your wallet from the header to get started.</p>
-        ) : (
-          <SimpleStorageSection /> // Show DApp interaction section if wallet is connected
-        )}
-      </main>
-    </div>
-  );
+  return <HomeClientSection />;
 }

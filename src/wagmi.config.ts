f10f2a44aa874e1dc09b3c659f6b33e8bb466119
John Titor <50095635+JohnRTitor@users.noreply.github.com>
2025-04-16 23:05:58 +0530

    Revert "validate env vars at runtime using zod"
    Doesn't work, maybe someday
    
    This reverts commit 46beec91743741fe2b0aeee06a4c0fa80aee0228.

--- src/wagmi.config.ts
+++ src/wagmi.config.ts
@@ -1,6 +1,5 @@
 import { createConfig, fallback, http, cookieStorage, createStorage } from "wagmi";
 import { mainnet, sepolia } from "wagmi/chains";
-import { env } from "@/lib/env";
 
 declare module "wagmi" {
   interface Register {
@@ -14,8 +13,14 @@ declare module "wagmi" {
 /// @dev: On Next, these environment variables have to be named
 /// @ with the prefix NEXT_PUBLIC_, else they will not be available in the browser
 /// and only be available in the server-side environment
-const sepoliaRpcUrl = env.NEXT_PUBLIC_SEPOLIA_RPC_URL;
-const mainnetRpcUrl = env.NEXT_PUBLIC_MAINNET_RPC_URL;
+const sepoliaRpcUrl = process.env.NEXT_PUBLIC_SEPOLIA_RPC_URL;
+const mainnetRpcUrl = process.env.NEXT_PUBLIC_MAINNET_RPC_URL;
+if (!sepoliaRpcUrl) {
+  console.error("Missing NEXT_PUBLIC_SEPOLIA_RPC_URL in environment variables.");
+}
+if (!mainnetRpcUrl) {
+  console.error("Missing NEXT_PUBLIC_MAINNET_RPC_URL in environment variables.");
+}
 
 export const config = createConfig({
   chains: [mainnet, sepolia],
